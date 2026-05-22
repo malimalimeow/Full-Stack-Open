d2e480c1f944bf437ccde674cf9c2beffba0e985
Heidy <heidy1443@hotmail.com>
2026-05-22 18:57:46 +0100

    3.10 update

--- part3/phonebookBE/index.js
+++ part3/phonebookBE/index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('dist'))
 
 
 let persons = [
@@ -105,10 +106,12 @@ app.post('/api/persons', (request, response) => {
     }
 
     persons = persons.concat(person)
+    console.log(person)
     response.json(person)
 })
 
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
