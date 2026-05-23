943b01b8b1aecb6f884eacd99f82d9dc2a13b157
Heidy <heidy1443@hotmail.com>
2026-05-23 23:38:40 +0100

    3.12 done

--- part3/phonebookBE/mongo.js
+++ part3/phonebookBE/mongo.js
@@ -20,29 +20,49 @@ const personSchema = new mongoose.Schema({
 
 const Persons = mongoose.model('Persons', personSchema)
 
-Persons.insertMany([
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]).then(result => {
-    console.log('note saved!')
-    return Persons.find({})
-}).then(found => {
-    console.log(found)
-    mongoose.connection.close()
-})
+Persons.find({}).then(result=>{
+
+    if(result.length===0){
+        Persons.insertMany([
+        {
+            "name": "Arto Hellas",
+            "number": "040-123456"
+        },
+        {
+            "name": "Ada Lovelace",
+            "number": "39-44-5323523"
+        },
+        {
+            "name": "Dan Abramov",
+            "number": "12-43-234345"
+        },
+        {
+            "name": "Mary Poppendieck",
+            "number": "39-23-6423122"
+        }
+    ]).then(()=>{console.log("Persons successfully initialized")
+        mongoose.connection.close()
+    })
+    }else if ((result.length > 0) && process.argv.length < 5) {
+        result.forEach(person => {
+            console.log(person)
+        })
+        mongoose.connection.close()
+    
+} else if (process.argv.length === 5) {
+    const newName = process.argv[3]
+    const newNumber = process.argv[4]
+    const person = new Persons({
+        name: newName,
+        number: newNumber
+    })
+    person.save().then(result => {
+        console.log(`added ${newName} number ${newNumber} to phonebook`)
+        mongoose.connection.close()
+    })
+
+}})
+
+
 
 
